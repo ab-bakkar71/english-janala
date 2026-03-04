fc25848f3042096dcbf8fd3b16b568ee78c1a31c
ab-bakkar71 <ab.bakkar420@gmail.com>
2026-03-04 11:48:41 +0600

    Vocabularies card section added with function

--- script/index.js
+++ script/index.js
@@ -3,7 +3,41 @@ const loadLessons = ()=>{
     fetch(AllLevelUrl)
     .then(res => res.json())
     .then(data => displayLessons(data.data))
-}
+};
+
+const loadWord =(id)=>{
+    const wordUrl = `https://openapi.programming-hero.com/api/level/${id}`
+    fetch(wordUrl)
+    .then(rsc=> rsc.json())
+    .then(json=> displayLoadWord(json.data))
+};
+
+const displayLoadWord = (words)=>{
+    const wordContainer = document.getElementById('word-container');
+    wordContainer.innerHTML= "";
+
+    words.forEach(word => {
+        const wordDiv= document.createElement('div');
+        wordDiv.innerHTML = `
+          <div class="bg-white rounded-lg text-center space-y-3 py-16 px-12 shadow-sm">
+          <h4 class="text-3xl font-bold">${word.word}</h4>
+          <p class="text-[20px] font-medium">${word.meaning}</p>
+          <h3 class="font-bangla text-3xl font-bold">${word.pronunciation}</h3>
+          <div class="flex justify-between items-center">
+            <button class="bg-[#1a90ff31] p-4 rounded-sm cursor-pointer hover:bg-[#1a90ffd2]"><i class="fa-solid fa-circle-info"></i></button>
+            <button class="bg-[#1a90ff31] p-4 rounded-sm cursor-pointer hover:bg-[#1a90ffd2]"><i class="fa-solid fa-volume-high"></i></button>
+          </div>
+        </div>
+        `
+        wordContainer.append(wordDiv)
+    })
+
+
+    
+
+
+};
+
 
 const displayLessons= (lessons)=>{
     // get the container
@@ -13,7 +47,7 @@ const displayLessons= (lessons)=>{
         const btnDiv = document.createElement('div');
         btnDiv.innerHTML=`<div class=" flex">  
         
-        <button href="#" class="btn btn-outline btn-primary"
+        <button onclick="loadWord(${lesson.level_no})" class="btn btn-outline btn-primary"
                   ><i class="fa-solid fa-book-open"></i> Lesson - ${lesson.level_no}</button>
        
        </div>
@@ -22,6 +56,6 @@ const displayLessons= (lessons)=>{
     }
 
 
-}
+};
 
 loadLessons()
