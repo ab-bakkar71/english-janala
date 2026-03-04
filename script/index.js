233668dffe21f8126a79f214052819aa3c9e591e
ab-bakkar71 <ab.bakkar420@gmail.com>
2026-03-04 12:18:14 +0600

    Fix Error

--- script/index.js
+++ script/index.js
@@ -16,13 +16,24 @@ const displayLoadWord = (words)=>{
     const wordContainer = document.getElementById('word-container');
     wordContainer.innerHTML= "";
 
+
+    if(words.length === 0){
+        wordContainer.innerHTML=`
+        <div class="py-16 text-center col-span-full space-y-3">
+          <img class="mx-auto" src="./assets/alert-error.png" alt="">
+          <p class="text-[#79716B] font-bangla">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+          <h3 class="font-bangla text-3xl font-medium">নেক্সট Lesson এ যান</h3>
+        </div>
+        `
+    }
+
     words.forEach(word => {
         const wordDiv= document.createElement('div');
         wordDiv.innerHTML = `
           <div class="bg-white rounded-lg text-center space-y-3 py-16 px-12 shadow-sm">
-          <h4 class="text-3xl font-bold">${word.word}</h4>
-          <p class="text-[20px] font-medium">${word.meaning}</p>
-          <h3 class="font-bangla text-3xl font-bold">${word.pronunciation}</h3>
+          <h4 class="text-3xl font-bold">${word.word ?word.word : 'শব্দ পাওয়া যায়নি'}</h4>
+          <p class="text-[20px] font-medium">${word.meaning ?word.meaning : 'অর্থ পাওয়া যায়নি'}</p>
+          <h3 class="font-bangla text-3xl font-bold">${word.pronunciation ?word.pronunciation : 'উচ্চারণ পাওয়া যায়নি'}</h3>
           <div class="flex justify-between items-center">
             <button class="bg-[#1a90ff31] p-4 rounded-sm cursor-pointer hover:bg-[#1a90ffd2]"><i class="fa-solid fa-circle-info"></i></button>
             <button class="bg-[#1a90ff31] p-4 rounded-sm cursor-pointer hover:bg-[#1a90ffd2]"><i class="fa-solid fa-volume-high"></i></button>
